fb79edadc0e073d7a49d48bf1cd623bcb4351d88
James Taggart (RIT Student) <jxt1607@ad.rit.edu>
2017-02-14 14:41:01 -0500

    Create Sunset testing scene. Get sun attached to light. Create procedural skybox. Create Lerp script.

---
+++ Assets/Materials/Skybox/SunSetting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSetting : MonoBehaviour {
+	public Vector3 sunRotation;
+	Quaternion newRotation;
+	public float speed = 0.1f;
+	Light lt;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (transform.position);
+		newRotation = Quaternion.Euler(sunRotation);
+		lt = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = Quaternion.Lerp (transform.rotation, newRotation, Time.time * speed);
+
+		if (transform.rotation == newRotation) {
+			lt.intensity = 0;
+		}
+
+	}
+}
