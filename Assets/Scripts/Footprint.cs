6dd4812f9a70e9bd3ca5bac8614df29ae7d60a0c
kxw1975 <kxw1975@rit.edu>
2017-02-19 17:25:28 -0500

    More Footprints!
    Actual footprints. Now with sounds for snow. Checked with tags. Fixed
    strange thing it did to model feet. Delayed destruction. Updated prefabs
    to have Footprint script and audio sources.

--- Assets/Scripts/Footprint.cs
+++ Assets/Scripts/Footprint.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Footprint : MonoBehaviour {
 
     Animator anim;  //Required for placing footesteps beneath feet
+    public AudioClip[] snowSteps;
+    public Material snowL;
+    public Material snowR;
+    public GameObject snowFootprintPrefab;
+    public AudioClip[] woodSteps;
+    public AudioClip[] crunchSteps;
+    public float lifetime = 20;
 
 	// Use this for initialization
 	void Start () {
@@ -49,42 +56,70 @@ public class Footprint : MonoBehaviour {
         Physics.Raycast(foot.position, -transform.up, out hit, 1);
         if (hit.collider != null)
         {
+            AudioSource audSou = foot.GetComponent<AudioSource>();
             switch (hit.transform.tag)
             {
                 case "Snow":
                     //play snow sound
-                    /*
-                    //location in world space of print
-                    Vector3 printLoc = hit.point + new Vector3(0, 0.01f, 0);
-                    Transform print = foot;
-                    print.position = printLoc;
-                    print.localScale = new Vector3(0.09f, 0.09f, 0.09f);//for plane -- might change
+                    audSou.clip = snowSteps[Random.Range(0, snowSteps.Length - 1)];
+                    audSou.Play();
 
-                    //create object for footprint -- make prefab with timer to delete?
-                    GameObject footprint = new GameObject("footprint");
-                    footprint.transform.position = print.position;
-                    footprint.transform.rotation = print.rotation;
-                    footprint.transform.localScale = print.localScale;
-                    */
+                    //create object for footprint
+                    GameObject footprint = Instantiate(snowFootprintPrefab);
+                    footprint.transform.position = hit.point + new Vector3(0, 0.01f, 0);
+                    footprint.transform.Rotate(0, foot.rotation.eulerAngles.y + 180, 0);
+                    if (foot.name == "Toe_L")
+                    {
+                        footprint.GetComponent<Renderer>().material = snowL;
+                    }
+                    else
+                    {
+                        footprint.transform.Rotate(0, 180, 0);
+                        footprint.GetComponent<Renderer>().material = snowR;
+                    }
+                    footprint.transform.rotation = Quaternion.FromToRotation(footprint.transform.up, hit.normal) * footprint.transform.rotation;
+                    Destroy(footprint, lifetime);
                     break;
                 case "Wood":
                     //play wood thump sound
+                    audSou.clip = woodSteps[Random.Range(0, woodSteps.Length - 1)];
+                    audSou.Play();
                     break;
                 case "Crunch":
                     //play crunch sound - ie. on glass or gravel
+                    audSou.clip = crunchSteps[Random.Range(0, crunchSteps.Length - 1)];
+                    audSou.Play();
                     break;
                 default:
                     //nothing...
+                    /*
                     Vector3 printLoc = hit.point + new Vector3(0, 0.01f, 0);
-                    Transform print = foot;
+                    Transform print = foot.transform;
+                    print.Rotate(new Vector3(0, 1, 0), 180);
                     print.position = printLoc;
-                    print.localScale = new Vector3(0.09f, 0.09f, 0.09f);
+                    print.localScale = new Vector3(0.018f, 0.09f, 0.018f);
+
+                    //random sound
+                    AudioSource audSou = foot.GetComponent<AudioSource>();
+                    audSou.clip = snowSteps[Random.Range(0, snowSteps.Length - 1)];
+                    audSou.Play();
 
                     //create object for footprint -- make prefab with timer to delete?
-                    GameObject footprint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    GameObject footprint = GameObject.CreatePrimitive(PrimitiveType.Plane);
                     footprint.transform.position = print.position;
                     footprint.transform.rotation = print.rotation;
                     footprint.transform.localScale = print.localScale;
+                    Material mat = footprint.GetComponent<Material>();
+                    if (foot.name == "Toe_L")
+                    {
+                        mat = snowL;
+                    }
+                    else
+                    {
+                        mat = snowR;
+                    }
+                    Destroy(footprint, 20);
+                    */
                     break;
             }
         }
