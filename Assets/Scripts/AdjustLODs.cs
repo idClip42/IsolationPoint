2c18c83a1e2a79103842dd97ae4b6fb6f27fec3b
Alexander Earley <axe6801@rit.edu>
2017-02-26 17:57:45 -0500

    Updated scaling on dining table prefab so that things don't warp when rotated Wrote an AdjustLODs script so that we can actually adjust the LODs, given that they're buried under multiple layers of prefabs so going through and changing them when needed is impractical. Some other minor updates

---
+++ Assets/Scripts/AdjustLODs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLODs : MonoBehaviour 
+{
+	/// <summary>
+	///  Changes all LODs that are children of this
+	/// </summary>
+
+
+	[Range(0, 1)] public float LOD1Percentage;
+	[Range(0, 1)] public float LOD2Percentage;
+
+	LODGroup[] lods;
+
+	void Start () 
+	{
+		lods = GetComponentsInChildren<LODGroup>();
+
+		for(int n = 0; n < lods.Length; ++n)
+		{
+			LODGroup l = lods[n];
+			LOD[] stuff = l.GetLODs();
+			stuff[0].screenRelativeTransitionHeight = LOD1Percentage;
+			if(stuff.Length > 2)
+				stuff[1].screenRelativeTransitionHeight = LOD2Percentage;
+			l.SetLODs(stuff);
+		}
+	}
+}
