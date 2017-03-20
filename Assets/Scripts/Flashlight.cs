4c6018c147b61c88d5b3fde12365e05ddf08d238
Alexander Earley <axe6801@rit.edu>
2017-03-20 18:49:15 -0400

    Added Flashlight Updated bed prefab so player can crawl under it

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour 
+{
+	Light light;
+	Collider col;
+
+	void Start () 
+	{
+		light = GetComponentInChildren<Light>();
+		col = GetComponent<Collider>();
+	}
+
+	public void PickUpPutDown(bool pickUp)
+	{
+		col.isTrigger = pickUp;
+		light.enabled = pickUp;
+	}
+}
