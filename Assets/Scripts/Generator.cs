1894d82227132cae89a3d3bcc0fd3f3915822cf5
Alexander Earley <axe6801@rit.edu>
2017-03-27 18:49:14 -0400

    Updated generator script to change material emission and have the user choose whether the lights start on or off.

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour {
 
+	public bool startLightsOn = true;
+
 	GameObject[] allLights;
 
 	void Start () 
 	{
 		allLights = GameObject.FindGameObjectsWithTag("ElectricLight");
 
-		TurnLights(true);
+		TurnLights(startLightsOn);
 	}
 
 	void TurnLights(bool on)
 	{
+		List <Material> materialList = new List<Material>();
 		for(int n = 0; n < allLights.Length; ++n)
 		{
 			allLights[n].GetComponentInChildren<Light>().enabled = on;
+			float intensity = 2.0f;
+			if(!on)
+				intensity = 0;
+			allLights[n].GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", new Color(1.0f, 1.0f, 1.0f, 1.0f) * intensity);
 		}
 	}
 }
