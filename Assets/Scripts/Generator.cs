d372a84ff0b8a531dfbe14967ee858f106136ca7
Alexander Earley <axe6801@rit.edu>
2017-03-27 19:02:17 -0400

    For debug, lights can be turned on and off with L key

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class Generator : MonoBehaviour {
 
 	public bool startLightsOn = true;
+	public bool debugLightsOnLKey = false;
 
 	GameObject[] allLights;
+	bool lightsOn;
 
 	void Start () 
 	{
 		allLights = GameObject.FindGameObjectsWithTag("ElectricLight");
+		lightsOn = false;
 
-		TurnLights(startLightsOn);
+		SwitchLights(startLightsOn);
 	}
 
-	void TurnLights(bool on)
+	void Update ()
 	{
+		if(debugLightsOnLKey == true)
+			if(Input.GetKeyDown(KeyCode.L))
+				SwitchLights(!lightsOn);
+	}
+
+	public void SwitchLights(bool lOn)
+	{
+		lightsOn = lOn;
 		List <Material> materialList = new List<Material>();
 		for(int n = 0; n < allLights.Length; ++n)
 		{
-			allLights[n].GetComponentInChildren<Light>().enabled = on;
+			allLights[n].GetComponentInChildren<Light>().enabled = lOn;
 			float intensity = 2.0f;
-			if(!on)
+			if(!lOn)
 				intensity = 0;
 			allLights[n].GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", new Color(1.0f, 1.0f, 1.0f, 1.0f) * intensity);
 		}
