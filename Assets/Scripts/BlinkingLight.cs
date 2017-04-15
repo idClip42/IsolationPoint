fdba0b1a764b3737f0aad5dde65aa144f692e232
kxw1975 <kxw1975@rit.edu>
2017-04-14 23:40:48 -0400

    More things to happen!
    -screen fades in from black
    -sound plays at start
    -antenna tower light blinks
    -updated nav mesh
    -enemy warps near basement once generator is fixed (doesn't target
    correctly yet)

---
+++ Assets/Scripts/BlinkingLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class BlinkingLight : MonoBehaviour {
+
+    Light bLight;    //The light to control the intensity of to make it 'blink'
+    float timer;    //x value in the sine function
+    public float flashSpeedMod = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        bLight = gameObject.GetComponent<Light>();
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        //sine function
+        bLight.intensity = 4 * Mathf.Sin(flashSpeedMod * timer) + 4;
+	}
+}
---
+++ Assets/Scripts/EventScripts/ScreenFadeEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Meant to be attached to a fullscreen image in the UI
+public class ScreenFadeEvent : Event {
+
+    Image img;
+    public float alpha = 1.0f;
+
+	// Use this for initialization
+	protected override void Start () {
+        //black screen
+        Color c = Color.black;
+        c.a = 1.0f;
+        //get image
+        img = GetComponent<Image>();
+        img.color = c;
+        //fade out img on start of game
+        Fade(alpha, timeToComplete);
+
+        base.Start();
+	}
+
+    void Fade(float alpha, float time)
+    {
+        img.CrossFadeAlpha(alpha, time, false);
+    }
+
+    public override void PlayEvent()
+    {
+        base.PlayEvent();
+        Fade(alpha, timeToComplete);
+    }
+}
