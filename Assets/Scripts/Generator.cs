402b6315fc8fee73dae0a86a0ff9fd842a9232b0
Alexander Earley <axe6801@rit.edu>
2017-04-21 16:24:42 -0400

    Updated light materials turning on and off

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -99,7 +99,7 @@ public class Generator : MonoBehaviour, IInteractable {
 			if(!lOn)
 				intensity = 0;
             allLights[n].GetComponentInChildren<Light>().intensity = intensity;
-			MeshRenderer m = allLights[n].GetComponent<MeshRenderer>();
+			MeshRenderer m = allLights[n].GetComponentInChildren<MeshRenderer>();
 			if(m != null) m.material.SetColor("_EmissionColor", new Color(1.0f, 1.0f, 1.0f, 1.0f) * intensity);
 		}
 	}
