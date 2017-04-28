fe5c7d393923ddbdad2392e4be7e9fd12fb13574
gmk1072 <gmk1072@rit.edu>
2017-04-28 14:33:50 -0400

    mannequins look at player

---
+++ Assets/Scripts/LookAtPlayerWhenNotLooking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayerWhenNotLooking : MonoBehaviour {
+
+    public Transform playerCamera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!GetComponent<Renderer>().isVisible)
+        {
+            Vector3 point = this.transform.position + Vector3.Normalize(playerCamera.transform.position - this.transform.position);
+            point.y = 0 + this.transform.position.y;
+            this.transform.LookAt(point);
+        }
+    }
+}
