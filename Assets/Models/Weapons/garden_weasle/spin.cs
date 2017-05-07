a2f3596ba3409402244bbf1fa37d6ce8882945e8
gmk1072 <gknotes2014@gmail.com>
2017-05-07 15:08:05 -0400

    garden weasle enemy weapon with particle effects, if you don't like em you can turn em off. i dont mind.

---
+++ Assets/Models/Weapons/garden_weasle/spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+    public float revolutionsPerSecond;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        this.transform.Rotate(new Vector3(0, 1, 0), revolutionsPerSecond * 6  + Random.Range(0,1));
+    }
+}
